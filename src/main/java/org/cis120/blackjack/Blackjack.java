a9b61e8465f5383af2a519d1a60d5b5196dc124b
taran317 <67604374+taran317@users.noreply.github.com>
2021-12-06 02:30:31 -0500

    added most blackjack logic

--- src/main/java/org/cis120/blackjack/Blackjack.java
+++ src/main/java/org/cis120/blackjack/Blackjack.java
@@ -28,90 +28,92 @@ import java.util.List;
  * visualized with Strings printed to the console.
  */
 public class Blackjack {
-
-    private int[][] board;
+    private Object[][] board;
+    private int[] bets;
+    private int[] money;
     private int numPlayers;
-    private int currTurn;
-    private int numTurns;
-    private boolean player1;
     private boolean gameOver;
     private List<Card> deck;
 
     /**
      * Constructor sets up game state.
      */
-    public Blackjack() {
-        reset(1);
+    public Blackjack(int players) {
+        money = new int[players];
+        for (int i = 0; i < money.length; i++) {
+            money[i] = 1000;
+        }
+        reset(players);
     }
 
     /**
-     * playTurn allows players to play a turn. Returns true if the move is
-     * successful and false if a player tries to play in a location that is
-     * taken or after the game has ended. If the turn is successful and the game
-     * has not ended, the player is changed. If the turn is unsuccessful or the
-     * game has ended, the player is not changed.
-     *
-     * @param c column to play in
-     * @param r row to play in
-     * @return whether the turn was successful
+     * reset (re-)sets the game state to start a new game.
      */
-    public boolean playTurn(int c, int r) {
-        if (board[r][c] != 0 || gameOver) {
-            return false;
+    public void reset(int players) {
+        board = new Ranks[15][players + 1];
+        bets = new int[players];
+        numPlayers = players;
+        gameOver = false;
+        deck = new ArrayList<>();
+        for (Suits s : Suits.values()) {
+            for (Ranks r : Ranks.values()) {
+                Card c = new Card(s, r);
+                deck.add(c);
+            }
         }
-
-        if (player1) {
-            board[r][c] = 1;
-        } else {
-            board[r][c] = 2;
+        Collections.shuffle(deck);
+        for (int i = 0; i < board[0].length; i++) {
+            board[0][i] = pop();
+            board[1][i] = pop();
         }
-
-        numTurns++;
-        if (checkWinner() == 0) {
-            player1 = !player1;
-        }
-        return true;
     }
 
-    public int calculateTotal(ArrayList<Card> cards) {
+    public void bet(int player, int amount) {
+        bets[player] = amount;
+        money[player] -= amount;
+    }
+
+    public int calculateTotal(int player) {
         int aces = 0;
         int total = 0;
-        for (Card c : cards) {
-            switch (c.getRank()) {
-                case TWO:
-                    total += 2;
-                    break;
-                case THREE:
-                    total += 3;
-                    break;
-                case FOUR:
-                    total += 4;
-                    break;
-                case FIVE:
-                    total += 5;
-                    break;
-                case SIX:
-                    total += 6;
-                    break;
-                case SEVEN:
-                    total += 7;
-                    break;
-                case EIGHT:
-                    total += 8;
-                    break;
-                case NINE:
-                    total += 9;
-                    break;
-                case TEN:
-                case KING:
-                case JACK:
-                case QUEEN:
-                    total += 10;
-                    break;
-                case ACE:
-                    total += 1;
-                    aces++;
-                    break;
+        for (int i = 0; i < board.length; i++) {
+            if (!board[i][player].equals(0) && board[i][player] != null) {
+                switch ((Ranks) board[i][player]) {
+                    case TWO:
+                        total += 2;
+                        break;
+                    case THREE:
+                        total += 3;
+                        break;
+                    case FOUR:
+                        total += 4;
+                        break;
+                    case FIVE:
+                        total += 5;
+                        break;
+                    case SIX:
+                        total += 6;
+                        break;
+                    case SEVEN:
+                        total += 7;
+                        break;
+                    case EIGHT:
+                        total += 8;
+                        break;
+                    case NINE:
+                        total += 9;
+                        break;
+                    case TEN:
+                    case KING:
+                    case JACK:
+                    case QUEEN:
+                        total += 10;
+                        break;
+                    case ACE:
+                        total += 1;
+                        aces++;
+                        break;
+                }
             }
         }
         for (int i = 0; i < aces; i++) {
@@ -123,27 +125,10 @@ public class Blackjack {
     }
 
     /**
-     * checkWinner checks whether the game has reached a win condition.
-     * checkWinner only looks for horizontal wins.
-     *
-     * @return 0 if nobody has won yet, 1 if player 1 has won, and 2 if player 2
-     * has won, 3 if the game hits stalemate
-     */
-    public int checkWinner() {
-        if (numTurns >= 9) {
-            gameOver = true;
-            return 3;
-        } else {
-            return 0;
-        }
-    }
-
-    /**
      * printGameState prints the current game state
      * for debugging.
      */
     public void printGameState() {
-        System.out.println("\n\nTurn " + numTurns + ":\n");
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 System.out.print(board[i][j]);
@@ -157,56 +142,62 @@ public class Blackjack {
         }
     }
 
+    public Ranks pop() {
+        Ranks r = null;
+        r = deck.get(0).getRank();
+        deck.remove(0);
+        return r;
+    }
+
     /**
-     * reset (re-)sets the game state to start a new game.
+     * @param player
+     * @return true if player bust or got a blackjack (turn ended)
      */
-    public void reset(int players) {
-        board = new int[15][players + 1];
-        numTurns = 0;
-        numPlayers = 0;
-        currTurn = 0;
-        player1 = true;
-        gameOver = false;
-        deck = new ArrayList<>();
-        for (Suits s : Suits.values()) {
-            for (Ranks r : Ranks.values()) {
-                Card c = new Card(s, r);
-                deck.add(c);
+    public boolean hit(int player) {
+        int newCardIndex = 0;
+        if(calculateTotal(player) == 21) {
+            return true;
+        }
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][player] == null) {
+                newCardIndex = i;
+                break;
             }
         }
-        Collections.shuffle(deck);
-    }
-
-    public void nextTurn() {
-        if (currTurn - 1 < numPlayers) {
-            currTurn++;
-        } else {
-            currTurn = 0;
+        board[newCardIndex + 1][player] = pop();
+        if (calculateTotal(player) > 21) {
+            bust(player);
+            return true;
         }
+        return false;
     }
 
-    /**
-     * getCurrentPlayer is a getter for the player
-     * whose turn it is in the game.
-     *
-     * @return true if it's Player 1's turn,
-     * false if it's Player 2's turn.
-     */
-    public boolean getCurrentPlayer() {
-        return player1;
+    public void bust(int player) {
+        board[0][player] = 0;
     }
 
-    /**
-     * getCell is a getter for the contents of the cell specified by the method
-     * arguments.
-     *
-     * @param c column to retrieve
-     * @param r row to retrieve
-     * @return an integer denoting the contents of the corresponding cell on the
-     * game board. 0 = empty, 1 = Player 1, 2 = Player 2
-     */
-    public int getCell(int c, int r) {
-        return board[r][c];
+    public int calculateDealerTotal() {
+        while (calculateTotal(0) <= 16) {
+            if (hit(0)) {
+                break;
+            }
+        }
+        return calculateTotal(0);
+    }
+
+    public void settle() {
+        for (int player = 1; player <= numPlayers; player++) {
+            if (board[0][0].equals(0) && !board[0][player].equals(0)) {
+                money[player] += 2 * bets[player];
+            } else {
+                int dealerTotal = calculateDealerTotal();
+                if (calculateTotal(player) > dealerTotal) {
+                    money[player] += 2 * bets[player];
+                } else if (calculateTotal(player) == dealerTotal) {
+                    money[player] += bets[player];
+                }
+            }
+        }
     }
 
     /**
@@ -220,36 +211,46 @@ public class Blackjack {
      * Run this file to see the output of this method in your console.
      */
     public static void main(String[] args) {
-        Blackjack t = new Blackjack();
+        Blackjack t = new Blackjack(5);
+        t.bet(1, 100);
+        t.bet(2, 200);
+        t.bet(3, 50);
+        t.bet(4, 100);
+        t.bet(5, 100);
+        for (int i = 0; i < 2; i++) {
+            if (t.hit(1)) {
+                break;
+            }
+        }
 
-        t.playTurn(1, 1);
-        t.printGameState();
+        for (int i = 0; i < 3; i++) {
+            if (t.hit(2)) {
+                break;
+            }
+        }
 
-        t.playTurn(0, 0);
-        t.printGameState();
+        for (int i = 0; i < 1; i++) {
+            if (t.hit(3)) {
+                break;
+            }
+        }
 
-        t.playTurn(0, 2);
-        t.printGameState();
+        for (int i = 0; i < 6; i++) {
+            if (t.hit(4)) {
+                break;
+            }
+        }
 
-        t.playTurn(2, 0);
-        t.printGameState();
+        for (int i = 0; i < 2; i++) {
+            if (t.hit(5)) {
+                break;
+            }
+        }
 
-        t.playTurn(1, 0);
-        t.printGameState();
+        t.settle();
 
-        t.playTurn(1, 2);
-        t.printGameState();
-
-        t.playTurn(0, 1);
-        t.printGameState();
-
-        t.playTurn(2, 2);
-        t.printGameState();
-
-        t.playTurn(2, 1);
-        t.printGameState();
         System.out.println();
         System.out.println();
-        System.out.println("Winner is: " + t.checkWinner());
+        System.out.println("Winner is: ");
     }
 }
